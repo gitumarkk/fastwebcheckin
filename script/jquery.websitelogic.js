0530b19ec4c3063afb8a38024565d69ad1a249b8
Mark Cafazzo <mark.cafazzo@gmail.com>
2013-09-12 00:46:51 -0400

    hosting scripts locally/ added ajax functions

--- script/jquery.websitelogic.js
+++ script/jquery.websitelogic.js
@@ -1,30 +1,47 @@
-
 /*global vars*/
-//var hi = false;
+var rootURL = 'http://fastwebcheckin.com/status/';
 
-$(document).on('pagebeforeshow', '#patientDetails', function () {
-    if ($('#container').find('cappy') == -1) {
-        if ($('#container').find('slappy') == -1) {
-            alert('Clinic is off-line (high v)');
-            //hi = true;
-        }
-        else { 
-            alert('Clinic is off-line (after h)');
-        }
-        //alert('Clinic is off-line');
-        //$('#details').hide();
-        //$.mobile.changePage('#offline', { transition: 'fade', changeHash: false });
-    }
-    else {
-        $('#container').remove();
-    }
-});
+$.ajaxSetup({
+    cache:false
+})
 
-$(document).on('pagebeforeshow', '#offline', function () {
-    if(hi == true){
-        $('#messageHighVolume').show();
-    }
-    else{
-        $('#messageAfterHours').show();
-    }
-});
+function getStatus(url){
+    alert('GET ' + url + ' clinic status...');
+    $.ajax({
+        url: rootURL + url,
+        dataType: "text/html",
+        success: renderResponse
+    });
+}
+
+function renderResponse(data){
+    alert('data: ' + data);
+    //var resp = data == null ? alert("no response") :  alert(data);
+}
+
+//$(document).on('pagebeforeshow', '#patientDetails', function () {
+//    if ($('#container').find('Off-line') == -1) {
+//        if ($('#container').find('High') == -1) {
+//            alert('Clinic is off-line (high v)');
+//            hi = true;
+//        }
+//        else { 
+//            alert('Clinic is off-line (after h)');
+//        }
+//        //alert('Clinic is off-line');
+//        //$('#details').hide();
+//        //$.mobile.changePage('#offline', { transition: 'fade', changeHash: false });
+//    }
+//    else {
+//        $('#container').remove();
+//    }
+//});
+
+//$(document).on('pagebeforeshow', '#offline', function () {
+//    if(hi == true){
+//        $('#messageHighVolume').show();
+//    }
+//    else{
+//        $('#messageAfterHours').show();
+//    }
+//});
