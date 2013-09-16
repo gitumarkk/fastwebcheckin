1848327d3c0bb5e2cc8dfe9c4529355767342d0e
Mark Cafazzo <mark.cafazzo@gmail.com>
2013-09-16 15:02:56 -0400

    terms of use page final

--- phonegapBuildService.bat
+++ phonegapBuildService.bat
@@ -1,1 +1,3 @@
 start https://build.phonegap.com/apps/546826/builds
+
+rem D:\documents\proj\fastwebcheckin\phonegapBuildService.bat
--- phonegapDeploy.bat
+++ phonegapDeploy.bat
@@ -1,2 +1,4 @@
 cd d:\dev\android\sdk\platform-tools\
 adb install d:\downloads\droidapps\fastwebcheckin-debug.apk
+
+rem D:\documents\proj\fastwebcheckin\phonegapDeploy.bat
