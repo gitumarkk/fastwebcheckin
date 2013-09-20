20a7648c93dfe2428d18364305247126de523d29
Mark Cafazzo <mark.cafazzo@gmail.com>
2013-09-20 17:56:31 -0400

    responsive with enquire.js

--- script/responsive.js
+++ script/responsive.js
@@ -1,6 +1,5 @@
 //iphone 3,4:
 enquire.register('screen and (max-width:320px)', {
-
     deferSetup: true,
     setup: function () {
         //load big screen content
@@ -13,4 +12,37 @@ enquire.register('screen and (max-width:320px)', {
         //show big screen content/apply styling
         $('#adFooter').css({ 'display': 'block' });
     }
-}, true);
+}, true)
+.register('screen and (orientation:portrait) and (max-width:720px) and (resolution: 316dpi)', {
+    match: function () {
+        alert('Galaxy Nexus portrait');
+    }
+})
+.register('screen and (orientation:landscape) and (max-width:720px) and (resolution: 316dpi)', {
+    match: function () {
+        alert('Galaxy Nexus landscape');
+    }
+});
+
+//.register('screen and (min-width:18.75em)', {
+//    match: function () {
+//        alert('300px breakpoint');
+//    }
+//})
+//.register('screen and (min-width:25em)', {
+//    match: function () {
+//        alert('400px breakpoint');
+//    }
+//})
+//.register('screen and (min-width:37.5em)', {
+//    match: function () {
+//        alert('600px breakpoint');
+//    }
+//})
+//.register('screen and (min-width:41em)', {
+//    match: function () {
+//        alert('tablet');
+//    }
+//});
+
+
