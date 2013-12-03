62ca1745f4d87aa7c7059abd2099441d9b644fb7
Mark Cafazzo <mark.cafazzo@gmail.com>
2013-12-02 23:45:31 -0500

    m

--- script/jquery.websitelogic.js
+++ script/jquery.websitelogic.js
@@ -1,48 +1,48 @@
 ///<reference path="script/jquery-2.0.3-vsdoc.js" />
 
 /*global vars*/
-var rootURL = 'http://dev.fastwebcheckin.com/check/submission';
+var rootURL = "http://dev.fastwebcheckin.com/check/submission";
 
 $.ajaxSetup({
     cache:false
 })
 
-function getStatus(url){
-    $.ajax({
-        url: rootURL + url,
-        dataType: "text/html",
-        success: renderResponse
-    });
-}
+function getPreviousCheckin(){
+    var jsonDate = localStorage["submittedDate"];
+    if (jsonDate != null && jsonDate.length > 0){
+        //alert('previous submit');
+        var checkinDate = new Date(parseInt(jsonDate) * 1000);
+        //var twoDaysAgo = new Date(2013, 11, 1);
+        //checkinDate = twoDaysAgo; 
+        var today = new Date();
+        if (Date.daysBetween(checkinDate, today) < 1){
+            var checkinStored = localStorage["checkinNumber"];
+            var locationStored = localStorage["locationName"]; 
+             
+            var message = "Clinic: " + locationStored + ".  Click the button below if you would like to submit a new check in.";
+            $("#checkinNo").val(checkinStored);
+            $("#attendTime").val(localStorage["attendTime"]);
+            $("#storedCheckinMessage").html(message);
+            $.mobile.changePage("#clearStorageDialog", { role: "page" });        
+        }
+    }
 
-function renderResponse(data){
-    alert('data: ' + data);
-    //var resp = data == null ? alert("no response") :  alert(data);
-}
+    //alert("today: " + today + " Checkin time: " + checkinDate + " Days between: " + Date.daysBetween(checkinDate, today));
+};
 
-//$(document).on('pagebeforeshow', '#patientDetails', function () {
-//    if ($('#container').find('Off-line') == -1) {
-//        if ($('#container').find('High') == -1) {
-//            alert('Clinic is off-line (high v)');
-//            hi = true;
-//        }
-//        else { 
-//            alert('Clinic is off-line (after h)');
-//        }
-//        //alert('Clinic is off-line');
-//        //$('#details').hide();
-//        //$.mobile.changePage('#offline', { transition: 'fade', changeHash: false });
-//    }
-//    else {
-//        $('#container').remove();
-//    }
-//});
+Date.daysBetween = function (date1, date2) {
+    //Get 1 day in milliseconds
+    var one_day = 1000 * 60 * 60 * 12;
 
-//$(document).on('pagebeforeshow', '#offline', function () {
-//    if(hi == true){
-//        $('#messageHighVolume').show();
-//    }
-//    else{
-//        $('#messageAfterHours').show();
-//    }
-//});
+    // Convert both dates to milliseconds
+    var date1_ms = date1.getTime();
+    var date2_ms = date2.getTime();
+
+    // Calculate the difference in milliseconds
+    var difference_ms = date2_ms - (date1_ms + (one_day * 4));
+
+    // Convert back to days and return
+    return Math.round(difference_ms / one_day);
+};
+
+
