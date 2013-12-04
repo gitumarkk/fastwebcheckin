a5b361bc324b45e25ddafd091b2e72315ae10ddc
Mark Cafazzo <mark.cafazzo@gmail.com>
2013-12-04 12:26:38 -0500

    production level changes

--- script/jquery.websitelogic.js
+++ script/jquery.websitelogic.js
@@ -1,4 +1,5 @@
-///<reference path="script/jquery-2.0.3-vsdoc.js" />
+///<reference path="jquery-2.0.3-vsdoc.js" />
+///<reference path="moment.min.js" />
 
 /*global vars*/
 var rootURL = "http://dev.fastwebcheckin.com/check/submission";
@@ -8,41 +9,93 @@ $.ajaxSetup({
 })
 
 function getPreviousCheckin(){
-    var jsonDate = localStorage["submittedDate"];
-    if (jsonDate != null && jsonDate.length > 0){
-        //alert('previous submit');
-        var checkinDate = new Date(parseInt(jsonDate) * 1000);
-        //var twoDaysAgo = new Date(2013, 11, 1);
-        //checkinDate = twoDaysAgo; 
+    var sdate = localStorage["submittedDate"];
+    var loc = localStorage["location"]; 
+
+    if (localStorage["terms"] == "YES"){
+        alert("getPreviousCheckin(): terms = YES");
+        restoreUserDetails();
+   
+        var checkinDate = new Date(parseInt(sdate) * 1000);
         var today = new Date();
-        if (Date.daysBetween(checkinDate, today) < 1){
-            var checkinStored = localStorage["checkinNumber"];
-            var locationStored = localStorage["locationName"]; 
-             
-            var message = "Clinic: " + locationStored + ".  Click the button below if you would like to submit a new check in.";
-            $("#checkinNo").val(checkinStored);
-            $("#attendTime").val(localStorage["attendTime"]);
-            $("#storedCheckinMessage").html(message);
-            $.mobile.changePage("#clearStorageDialog", { role: "page" });        
+        var now = moment();
+        var endOfDay = moment(checkinDate).endOf('day').subtract('hours', 7).fromNow();
+
+        if (Date.daysBetween(today, checkinDate) < 0.3){
+            alert(" Checkin time: " + checkinDate
+            + ". \n Today: " + now.toLocaleString() + "\n Days between: "
+            + Date.daysBetween(today, checkinDate)
+            + "\n Hours (since checkin) until end of day: "
+            + endOfDay);
+
+            fillConfirmationMessage();
+            alert("getPreviousCheckin(): switching to patientDetails page");
+            $.mobile.changePage("#patientDetails", { role: "page" });      
         }
     }
-
-    //alert("today: " + today + " Checkin time: " + checkinDate + " Days between: " + Date.daysBetween(checkinDate, today));
 };
 
 Date.daysBetween = function (date1, date2) {
-    //Get 1 day in milliseconds
-    var one_day = 1000 * 60 * 60 * 12;
+    var oneDay = 1000 * 60 * 60 * 24;
 
-    // Convert both dates to milliseconds
-    var date1_ms = date1.getTime();
-    var date2_ms = date2.getTime();
+    var date1MS = date1.getTime();
+    var date2MS = date2.getTime();
 
-    // Calculate the difference in milliseconds
-    var difference_ms = date2_ms - (date1_ms + (one_day * 4));
-
-    // Convert back to days and return
-    return Math.round(difference_ms / one_day);
+    var differenceMS = date1MS - date2MS;
+    return differenceMS / oneDay;
 };
+
+function storeUserDetails() { 
+    localStorage["priorityCode"] = $('#priorityCode').val();
+    localStorage["firstName"] = $('#firstName').val();
+    localStorage["lastName"] = $('#lastName').val();
+    localStorage["emailAddress"] = $('#emailAddress').val();
+    localStorage["postalCode"] = $('#postalCode').val();
+}
+
+function restoreUserDetails(){
+    var pCode = localStorage["priorityCode"];
+    if (pCode != null) {
+        $('#priorityCode').val(localStorage["priorityCode"]);
+    }
+
+    $('#firstName').val(localStorage["firstName"]);
+    $('#lastName').val(localStorage["lastName"]);
+    $('#emailAddress').val(localStorage["emailAddress"]);
+    $('#postalCode').val(localStorage["postalCode"]);
+}
+
+function fillConfirmationMessage() { 
+    $("#checkinNumber").html(localStorage["checkinNumber"]);
+    $("#attendTimeConfirmation").html(localStorage["attendTime"]);
+
+    $("#locationConfirmation").html(localStorage["locationFriendlyName"]);
+    $("#firstNameConfirmation").html(localStorage["firstName"]);
+    $("#lastNameConfirmation").html(localStorage["lastName"]);
+    $("#submittedDateConfirmation").html(localStorage["submitDateFormatted"]);
+    $("#emailConfirmation").html(localStorage["emailAddress"]);
+    $("#postalCodeConfirmation").html(localStorage["postalCode"]);
+
+    if (localStorage["location"] == "pcnniagara") {
+        localStorage["clinicAddress"] = "6150 Valley Way Suite 104 Niagara Falls ON L2E 1Y3";
+        localStorage["clinicMapLinkHref"] = "http://maps.google.ca/maps?hl=en&q=6150+Valley+Way,+Niagara+Falls,+Niagara+Regional+Municipality,+Ontario&safe=active&ie=UTF8&cd=1&geocode=FcGekQIdCBRJ-w&split=0&sll=49.891235,-97.15369&sspn=16.71875,56.536561&ll=43.097793,-79.096824&spn=0.023878,0.028839&z=15"
+    }
+    else {
+        localStorage["clinicAddress"] = "800 Niagara Street North Unit G1 Welland ON L3C 5Z4";
+        localStorage["clinicMapLinkHref"] = "http://maps.google.ca/maps?f=q&source=s_q&hl=en&geocode=&q=800+Niagara+Street,+North,+Suite+G1.+welland,+Ontario&sll=43.088688,-79.052612&sspn=0.011941,0.01442&g=800+Niagara+Street,+North,+Suite+G1&ie=UTF8&ll=43.016509,-79.249792&spn=0.04782,0.057678&z=14";
+    }
+    $("#clinicAddress").html(localStorage["clinicAddress"]);
+    $("#clinicMapLinkHref").attr("href", localStorage["clinicMapLinkHref"]);
+}
+
+function clearLocalStorage() {
+    alert("cleared local storage");
+    localStorage["checkinNumber"] = "";
+    localStorage["attendTime"] = "";
+    localStorage["submittedDate"] = "";
+    localStorage["emailPermission"] = "NO";
+    localStorage["readInstructions"] = "NO";
+    localStorage["terms"] = "NO";
+}
 
 
