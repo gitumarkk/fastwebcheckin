6be482fd3b69da601773ef4e43cb74b98562d6b7
Mark Cafazzo <mark.cafazzo@gmail.com>
2013-12-13 16:43:27 -0500

    added locationFriendlyName handling on previous checkin detection

--- script/jquery.applogic.js
+++ script/jquery.applogic.js
@@ -92,7 +92,6 @@ function fillConfirmationMessage() {
     $("#attendTimeConfirmation").html(localStorage["attendTime"]);
     $("#attendInstructions").html(localStorage["confirmMessage"]);
 
-    $("#locationConfirmation").html(localStorage["locationFriendlyName"]);
     $("#firstNameConfirmation").html(localStorage["firstName"]);
     $("#lastNameConfirmation").html(localStorage["lastName"]);
 
@@ -105,10 +104,12 @@ function fillConfirmationMessage() {
     if (localStorage["location"] == "pcnniagara") {
         localStorage["clinicAddress"] = "6150 Valley Way Suite 104 Niagara Falls ON L2E 1Y3";
         localStorage["clinicMapLinkHref"] = "http://maps.google.ca/maps?hl=en&q=6150+Valley+Way,+Niagara+Falls,+Niagara+Regional+Municipality,+Ontario&safe=active&ie=UTF8&cd=1&geocode=FcGekQIdCBRJ-w&split=0&sll=49.891235,-97.15369&sspn=16.71875,56.536561&ll=43.097793,-79.096824&spn=0.023878,0.028839&z=15"
+        localStorage["locationFriendlyName"] = "PCN Niagara";
     }
     else {
         localStorage["clinicAddress"] = "800 Niagara Street North Unit G1 Welland ON L3C 5Z4";
         localStorage["clinicMapLinkHref"] = "http://maps.google.ca/maps?f=q&source=s_q&hl=en&geocode=&q=800+Niagara+Street,+North,+Suite+G1.+welland,+Ontario&sll=43.088688,-79.052612&sspn=0.011941,0.01442&g=800+Niagara+Street,+North,+Suite+G1&ie=UTF8&ll=43.016509,-79.249792&spn=0.04782,0.057678&z=14";
+        localStorage["locationFriendlyName"] = "PCN Welland";
     }
     $("#clinicAddress").html(localStorage["clinicAddress"]);
     $("#clinicMapLinkHref").attr("href", localStorage["clinicMapLinkHref"]);
